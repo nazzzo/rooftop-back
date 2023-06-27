25cad3334f05aa33baf87c586f68336f7f93155e
parkjonghwan22 <gch02933@naver.com>
2023-06-27 11:22:48 +0900

    feat: 오류 작업

--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,55 +1,59 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Collection } from './schemas/collection.schema';
 import { CollectionDto } from './dto/collection.dto';
 import { ConfigService } from '@nestjs/config';
 
-
 @Injectable()
 export class CollectionService {
-    constructor(
-        @InjectModel(Collection.name)
-        private collectionModel: mongoose.Model<Collection>,
-        private configService: ConfigService
-    ) { }
+  constructor(
+    @InjectModel(Collection.name)
+    private collectionModel: mongoose.Model<Collection>,
+    private configService: ConfigService,
+  ) {}
 
-    async getCollection(address: string): Promise<Collection[]> {
-        try {
-            const collection = await this.collectionModel.find({ creator: address });
-            console.log(`get collection::: ${collection}`);
-            if (!collection) {
-                throw new NotFoundException('Collection not found');
-            }
-            return collection;
-        } catch (e) {
-            throw new Error(e);
-        }
+  async getCollection(address: string): Promise<Collection[]> {
+    try {
+      const collection = await this.collectionModel.find({
+        $or: [{ creator: address }, { address }],
+      });
+      console.log(`get collection::: ${collection}`);
+      if (!collection) {
+        throw new NotFoundException('Collection not found');
+      }
+      return collection;
+    } catch (e) {
+      throw new Error(e);
     }
+  }
 
-    async find(body: CollectionDto): Promise<Collection> {
-        try {
-            const client = this.configService.get('APP_URL')
-            const { name, symbol, url } = body;
-            const checkDuplicate = await this.collectionModel.findOne({
-                $or: [{ name }, { symbol }, { url: `${client}/collections/${url}` }],
-            });
-            return checkDuplicate
-
-        } catch (e) {
-            throw new Error(e);
-        }
+  async find(body: CollectionDto): Promise<Collection> {
+    try {
+      const client = this.configService.get('APP_URL');
+      const { name, symbol, url } = body;
+      const checkDuplicate = await this.collectionModel.findOne({
+        $or: [{ name }, { symbol }, { url: `${client}/collections/${url}` }],
+      });
+      return checkDuplicate;
+    } catch (e) {
+      throw new Error(e);
     }
+  }
 
-    async create(collectionDto: CollectionDto): Promise<Collection> {
-        const client = this.configService.get('APP_URL')
+  async create(collectionDto: CollectionDto): Promise<Collection> {
+    const client = this.configService.get('APP_URL');
 
-        const newCollection = await this.collectionModel.create({
-            ...collectionDto,
-            url: `${client}/collections/${collectionDto.url}`
-        })
-        console.log(newCollection)
+    const newCollection = await this.collectionModel.create({
+      ...collectionDto,
+      url: `${client}/collections/${collectionDto.url}`,
+    });
+    console.log(newCollection);
 
-        return newCollection
-    }
+    return newCollection;
+  }
 }
